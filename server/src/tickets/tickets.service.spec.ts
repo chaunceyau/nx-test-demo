1160ff89bc8f10866b6236a2154bc61cc610717d
Austin Chauncey <austinchauncey@chief.com>
2023-12-01 10:36:04 -0600

    test: add basic service case for tickets

--- server/src/tickets/tickets.service.spec.ts
+++ server/src/tickets/tickets.service.spec.ts
@@ -11,9 +11,95 @@ describe('TicketsService', () => {
     }).compile();
 
     service = module.get<TicketsService>(TicketsService);
+
+    service['storedTickets'] = [
+      {
+        id: 1,
+        description: 'Ticket 1',
+        assigneeId: 1,
+        completed: false,
+      },
+      {
+        id: 2,
+        description: 'Ticket 2',
+        assigneeId: 1,
+        completed: false,
+      },
+      {
+        id: 3,
+        description: 'Ticket 3',
+        assigneeId: 2,
+        completed: true,
+      },
+      {
+        id: 4,
+        description: 'Ticket 4',
+        assigneeId: 2,
+        completed: false,
+      },
+      {
+        id: 5,
+        description: 'Ticket 5',
+        assigneeId: 3,
+        completed: true,
+      },
+      {
+        id: 6,
+        description: 'Ticket 6',
+        assigneeId: 3,
+        completed: false,
+      },
+      {
+        id: 7,
+        description: 'Ticket 7',
+        assigneeId: 1,
+        completed: true,
+      },
+      {
+        id: 8,
+        description: 'Ticket 8',
+        assigneeId: 2,
+        completed: false,
+      },
+      {
+        id: 9,
+        description: 'Ticket 9',
+        assigneeId: 3,
+        completed: true,
+      },
+      {
+        id: 10,
+        description: 'Ticket 10',
+        assigneeId: 1,
+        completed: false,
+      },
+    ];
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('tickets', () => {
+    it('should return all tickets when no filters are provided', async () => {
+      const result = await service.tickets();
+      expect(result).toEqual(service['storedTickets']);
+    });
+
+    it('should return only completed tickets when "complete" filter is provided', async () => {
+      const result = await service.tickets({ status: 'complete' });
+      const completedTickets = service['storedTickets'].filter(
+        (ticket) => ticket.completed
+      );
+      expect(result).toEqual(completedTickets);
+    });
+
+    it('should return only incomplete tickets when "incomplete" filter is provided', async () => {
+      const result = await service.tickets({ status: 'incomplete' });
+      const incompleteTickets = service['storedTickets'].filter(
+        (ticket) => !ticket.completed
+      );
+      expect(result).toEqual(incompleteTickets);
+    });
   });
 });
